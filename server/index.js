60bf88d46c8f9d09995d07c0d81c27c97a64b277
Aghara Mitul J. <agharamitul27@icloud.com>
2026-06-24 12:30:43 +0530

    Add backend vercel configuration and export app

--- server/index.js
+++ server/index.js
@@ -480,3 +480,5 @@ app.get('/api/seed', async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
